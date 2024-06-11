88378369ba657f4f45ca72aaa129fe380107b08b
kimseongmin <naakoko47@gmail.com>
2024-06-12 00:15:08 +0900

    @SpringBootApplication(exclude = SecurityAutoConfiguration.class) <- 를 추가하여 html들어가면 시큐리티 인증페이지 뜨던걸 api넘김. (이걸 등록해야지만 api사용하기위한 인증 페이지가 안뜸)

--- src/main/java/com/terabox/demo/DemoApplication.java
+++ src/main/java/com/terabox/demo/DemoApplication.java
@@ -2,8 +2,9 @@ package com.terabox.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // security api를 인증없이 접근하려면 얘가 필요함
 public class DemoApplication {
 
     public static void main(String[] args) {
