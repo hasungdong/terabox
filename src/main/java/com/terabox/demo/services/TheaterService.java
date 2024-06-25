635328758e0962da7c23b01a21908abba82524f2
hasungdong <gktjdehd3333@gmail.com>
2024-06-26 03:13:17 +0900

    0625 지역에 마우스 갖다대면 지점들 나오게 만들었음 이제 지도랑 연결하면 됨

--- src/main/java/com/terabox/demo/services/TheaterService.java
+++ src/main/java/com/terabox/demo/services/TheaterService.java
@@ -2,7 +2,6 @@ package com.terabox.demo.services;
 
 import com.terabox.demo.entities.TheaterEntity;
 import com.terabox.demo.mappers.TheaterMapper;
-import com.terabox.demo.results.CommonResult;
 import com.terabox.demo.vos.TheaterVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
