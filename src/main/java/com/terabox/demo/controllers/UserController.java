440f900c2eb0015b439ff4c8dfc2bbb943d464d2
kimseongmin <naakoko47@gmail.com>
2024-07-03 12:32:00 +0900

    인증번호를 보내야하는데 예외처리가 안되있었음 NoSuchAlgorithmException 추가함

--- src/main/java/com/terabox/demo/controllers/UserController.java
+++ src/main/java/com/terabox/demo/controllers/UserController.java
@@ -71,7 +71,7 @@ public class UserController {
 
     @PostMapping(value = "resetPasswordEmail", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public String postResetPasswordEmail(EmailAuthEntity emailAuth) throws MessagingException {
+    public String postResetPasswordEmail(EmailAuthEntity emailAuth) throws MessagingException, NoSuchAlgorithmException {
         Result result = this.userService.sendResetPasswordEmail(emailAuth);
         JSONObject responseObject = new JSONObject();
         responseObject.put("result", result.name().toLowerCase());
